724de1aff62867ff0616b8a246f77a53201ffb13
Amine-jabote <114097461+Amine-jabote@users.noreply.github.com>
2024-05-04 16:21:21 +0200

    la mise à jour du  Metier_information.py

--- Metier_information.py
+++ Metier_information.py
@@ -1,3 +1,7 @@
+from extraire_metier_demande import extraire_metier_demande
+from trouver_matieres_par_metier import trouver_matieres_par_metier
+from trouver_matieres_par_metier import generer_reponse_matieres_par_metier
+
 def traiter_intention_metiers(phrase):
     metier_demande = extraire_metier_demande(phrase)
     matieres_trouvees = trouver_matieres_par_metier(metier_demande)
