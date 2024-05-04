6ab03ccaaf9cf6309580b917b01b4fec3bc3c17c
Amine-jabote <114097461+Amine-jabote@users.noreply.github.com>
2024-05-04 16:23:19 +0200

    Mise à jour du extraire_UE.py

--- extraire_UE.py
+++ extraire_UE.py
@@ -6,8 +6,18 @@ with open('D:/Devops/Chatbutt/db.json', 'r', encoding='utf-8' ) as file:
     
 # Fonction pour extraire les informations d'une matière
 def extraire_info_matiere(code_matiere):
+    code_matiere = code_matiere.upper()
     print(code_matiere)
     if code_matiere in matieres_database:
         return matieres_database[code_matiere]
     else:
         return None
+    
+def extraire_metiers_matiere(code_matiere):
+    code_matiere = code_matiere.upper()
+    if code_matiere in matieres_database:
+        matiere_info = matieres_database[code_matiere]
+        metiers_associés = matiere_info.get("Métiers", [])
+        return metiers_associés
+    else:
+        return []
